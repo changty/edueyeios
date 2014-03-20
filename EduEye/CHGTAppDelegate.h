f10e949ce348a17886f5bdc1586eea8770f5fa75
Einari Kurvinen <einari.kurvinen@gmail.com>
2014-03-20 15:44:54 +0200

    Added icons

--- EduEye/CHGTAppDelegate.h
+++ EduEye/CHGTAppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CHGTViewController.h"
 
 @interface CHGTAppDelegate : UIResponder <UIApplicationDelegate>
 
--- EduEye/CHGTAppDelegate.m
+++ EduEye/CHGTAppDelegate.m
@@ -20,6 +20,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -27,6 +28,9 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
+    CHGTViewController *vc = [[CHGTViewController alloc] init];
+    [vc stopImageStreaming];
+    [vc stopServer];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -37,6 +41,9 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    CHGTViewController *vc = [[CHGTViewController alloc] init];
+    [vc startImageStreaming];
+    [vc startServer];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
